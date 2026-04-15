ef7bb119e1becc9488d7c41bce785fb46b09506e
Skaro7 <marc_tau72@hotmail.com>
2026-04-15 11:02:44 +0200

    style: typo

--- src/main/java/cat/itacademy/blackjack/domain/model/Game.java
+++ src/main/java/cat/itacademy/blackjack/domain/model/Game.java
@@ -99,7 +99,7 @@ public class Game {
         }
     }
 
-        public String getId()                  { return id; }
+    public String getId()                  { return id; }
     public void setId(String id)           { this.id = id; }
     public Long getPlayerId()              { return playerId; }
     public void setPlayerId(Long playerId) { this.playerId = playerId; }
